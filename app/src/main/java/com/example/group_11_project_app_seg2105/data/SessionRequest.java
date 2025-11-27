053420ec0e4bdaf965252f594e973bb158dc157a
AtiqueAqtab <164593387+AtiqueAqtab@users.noreply.github.com>
2025-11-26 21:59:18 -0500

    updated session request for temporary testing
    added temporary new constructor for testing

--- app/src/main/java/com/example/group_11_project_app_seg2105/data/SessionRequest.java
+++ app/src/main/java/com/example/group_11_project_app_seg2105/data/SessionRequest.java
@@ -3,7 +3,7 @@ package com.example.group_11_project_app_seg2105.data;
 public class SessionRequest {
 
     public final long id;
-    public final long slotId;          // NEW – required for Part 5
+    public final long slotId;          // NEW – required for Part 5 will be -1 for now (no FK column yet)
     public final String studentEmail;
     public final String tutorEmail;
     public final String date;
@@ -28,5 +28,10 @@ public class SessionRequest {
         this.start = start;
         this.end = end;
         this.status = status;
+    }
+
+    // Convenience constructor used by current DatabaseHelper
+    public SessionRequest(long id, String studentEmail, String tutorEmail, String date, String start, String end, String status) {
+        this(id, -1L, studentEmail, tutorEmail, date, start, end, status);
     }
 }
